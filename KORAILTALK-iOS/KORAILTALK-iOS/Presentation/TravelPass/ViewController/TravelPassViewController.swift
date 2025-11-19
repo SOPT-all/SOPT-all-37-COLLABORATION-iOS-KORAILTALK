4edc1736e811afa798f5c3190040ebba0ade284c
hyunseo <ols3040015@naver.com>
2025-11-20 01:27:10 +0900

    [feat] TabBarController와 더미 ViewControllers 추가

---
+++ KORAILTALK-iOS/KORAILTALK-iOS/Presentation/TravelPass/ViewController/TravelPassViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TravelPassViewController.swift
+//  KORAILTALK-iOS
+//
+//  Created by 한현서 on 11/20/25.
+//
+
+import Foundation
+import UIKit
+
+final class TravelPassViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gray100
+        title = "여행상품 패스"
+    }
+}
