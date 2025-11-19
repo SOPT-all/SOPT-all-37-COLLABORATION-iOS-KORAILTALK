8b433d529e2e88bf47810aba17ce3acb7cd938a4
JaeSunEo <wotjs020708@gmail.com>
2025-11-20 01:15:47 +0900

    [fix/#11] 코드 리뷰 반영

--- KORAILTALK-iOS/KORAILTALK-iOS/Global/Components/TrainTagButton.swift
+++ KORAILTALK-iOS/KORAILTALK-iOS/Global/Components/TrainTagButton.swift
@@ -10,7 +10,7 @@ import UIKit
 final class TrainTagButton: UIButton {
     
     
-    var type: TrainTagType? {
+    var type: TrainTagType {
         didSet {
             updateStyle()
         }
@@ -22,49 +22,49 @@ final class TrainTagButton: UIButton {
         }
     }
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setLayout()
-    }
-    
-    convenience init(type: TrainTagType, isSelected: Bool = false) {
-        self.init(frame: .zero)
+    init(type: TrainTagType, isSelected: Bool = false) {
         self.type = type
+        super.init(frame: .zero)
         self.isSelected = isSelected
+        setLayout()
+        updateStyle()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override var intrinsicContentSize: CGSize {
-        let labelSize = titleLabel?.intrinsicContentSize ?? .zero
-        
-        return CGSize(width: labelSize.width + 24, height: labelSize.height + 16)
-    }
-    
     private func setLayout() {
-        self.layer.cornerRadius = 4
-        self.titleLabel?.font = .body2_m_15
-        
+        var config = UIButton.Configuration.filled()
+        config.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 12, bottom: 8, trailing: 12)
+        config.cornerStyle = .fixed
+        config.background.cornerRadius = 4
+        config.titleTextAttributesTransformer = UIConfigurationTextAttributesTransformer{ incoming in
+            var outgoing = incoming
+            outgoing.font = .body2_m_15
+            return outgoing
+        }
+        self.configuration = config
     }
     
     private func updateStyle() {
-            guard let type = type else { return }
-            
-            self.setTitle(type.title, for: .normal)
-            
-            let style = type.style(isSelected: self.isSelected)
-            
-            self.backgroundColor = style.backgroundColor
-            self.setTitleColor(style.titleColor, for: .normal)
-            
-            if let borderColor = style.borderColor {
-                self.layer.borderColor = borderColor.cgColor
-                self.layer.borderWidth = 1.0
-            } else {
-                self.layer.borderWidth = 0.0
-            }
+        var config = self.configuration ?? UIButton.Configuration.filled()
+        config.title = type.title
+        
+        let style = type.style(isSelected: self.isSelected)
+        
+        config.baseForegroundColor = style.titleColor
+        config.background.backgroundColor = style.backgroundColor
+    
+        if let borderColor = style.borderColor {
+            config.background.strokeColor = borderColor
+            config.background.strokeWidth = 1
+        } else {
+            config.background.strokeColor = .clear
+            config.background.strokeWidth = 0
         }
 
+        self.configuration = config
+        
+    }
 }
