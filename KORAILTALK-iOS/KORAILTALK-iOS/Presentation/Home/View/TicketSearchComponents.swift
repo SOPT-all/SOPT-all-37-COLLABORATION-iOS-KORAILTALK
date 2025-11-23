cb7c157fd2babefd89d7d88229949cbcb7901c41
hyunseo <ols3040015@naver.com>
2025-11-24 00:58:59 +0900

    [feat] InfoView 구현

--- KORAILTALK-iOS/KORAILTALK-iOS/Presentation/Home/View/TicketSearchComponents.swift
+++ KORAILTALK-iOS/KORAILTALK-iOS/Presentation/Home/View/TicketSearchComponents.swift
@@ -53,4 +53,46 @@ final class StationRowView: BaseView{
 }
 
 
+// MARK: - InfoRowView (날짜/인원)
 
+final class InfoRowView: BaseView{
+    
+    private let iconImageView = UIImageView().then{
+        $0.contentMode = .scaleAspectFit
+        $0.tintColor = .gray300
+    }
+    
+    private let contentLabel = UILabel().then {
+        $0.font = .sub3_m_16
+        $0.textColor = .mainBlack
+    }
+    
+    init(iconName: String, content: String) {
+        super.init(frame: .zero)
+        iconImageView.image = UIImage(systemName: iconName)
+        contentLabel.text = content
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func setUI() {
+        addSubview(iconImageView)
+        addSubview(contentLabel)
+    }
+    
+    override func setLayout() {
+        iconImageView.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(20)
+            $0.centerY.equalToSuperview()
+            $0.size.equalTo(24)
+        }
+        
+        contentLabel.snp.makeConstraints {
+            $0.leading.equalTo(iconImageView.snp.trailing).offset(20)
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(20)
+        }
+    }
+}
