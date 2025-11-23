bbe4051e36aeecd1af1b2a556571559bc9abed8d
hyunseo <ols3040015@naver.com>
2025-11-23 23:32:58 +0900

    [fix] SceneDelegate 수정

--- KORAILTALK-iOS/KORAILTALK-iOS/Application/SceneDelegate.swift
+++ KORAILTALK-iOS/KORAILTALK-iOS/Application/SceneDelegate.swift
@@ -14,7 +14,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let vc = UINavigationController(rootViewController: HomeViewController())
+        let factory = AppFactory()
+        let vc = UINavigationController(rootViewController: TabBarController(factory: factory))
         window.rootViewController = vc
         self.window = window
         window.makeKeyAndVisible()
