4a30dfc466f64f2910cd187f319b16096ff1a9bb
sum130 <sm3904@naver.com>
2025-11-24 16:36:02 +0900

    [feat]OKButton 컴포넌트

---
+++ KORAILTALK-iOS/KORAILTALK-iOS/Global/Components/OKButton.swift
@@ -0,0 +1,71 @@
+//
+//  OKButton.swift
+//  KORAILTALK-iOS
+//
+//  Created by sumin Kong on 11/24/25.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class OKButton: UIButton {
+    private let textLabel = UILabel()
+    
+    var isCheckd: Bool = false {
+        didSet {
+            updateStyle()
+        }
+    }
+    
+    private let fixedSize = CGSize(width: 56, height: 36)
+    
+    override init(frame: CGRect){
+        let fixedFrame = CGRect(origin: frame.origin, size: fixedSize)
+        super.init(frame: fixedFrame)
+        setUI()
+        setStyle()
+        setLayout()
+        setupButton()
+    }
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.frame.size = fixedSize
+        setupButton()
+    }
+    
+    func setStyle(){
+        layer.cornerRadius = 8
+        textLabel.font = .body1_r_16
+        textLabel.textColor = .mainWhite
+        textLabel.text = "확인"
+    }
+    func setUI() {
+        addSubview(textLabel)
+    }
+    func setLayout() {
+        textLabel.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(14)
+            $0.top.bottom.equalToSuperview().inset(7)
+        }
+    }
+    
+    private func setupButton() {
+        updateStyle()
+        self.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+    }
+        
+    @objc
+    func buttonTapped() {
+        self.isCheckd.toggle()
+    }
+    
+    private func updateStyle() {
+        if isCheckd {
+            self.backgroundColor = .primary300
+        }else {
+            self.backgroundColor = .gray300
+        }
+    }
+}
