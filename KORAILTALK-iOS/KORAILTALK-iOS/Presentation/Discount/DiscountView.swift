2131a78468308171b966ae9de750a22bf06458a8
sum130 <sm3904@naver.com>
2025-11-25 00:57:19 +0900

    [fix/49] DiscountView 코드 정리

--- KORAILTALK-iOS/KORAILTALK-iOS/Presentation/Discount/DiscountView.swift
+++ KORAILTALK-iOS/KORAILTALK-iOS/Presentation/Discount/DiscountView.swift
@@ -10,7 +10,7 @@ import UIKit
 import SnapKit
 import Then
 
-class DisCountView: BaseView {
+final class DiscountView: BaseView {
     
     private let veteransLabel = UILabel().then{
         $0.font = .sub3_m_16
@@ -31,32 +31,14 @@ class DisCountView: BaseView {
         $0.distribution = .equalSpacing
         $0.addArrangedSubviews(veteransLabel, veteransTextField)
     }
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setUI()
-        setStyle()
-        setLayout()
-    }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-        
-    /// UI 컴포넌트 속성 설정 (do 메서드 관련)
-    override func setStyle() {}
-        
-    /// UI 위계 설정 (addSubView 등)
     override func setUI() {
         addSubviews(veteransStack)
     }
-        
-    /// 오토레이아웃 설정 (SnapKit 코드 관련)
     override func setLayout() {
         veteransStack.snp.makeConstraints {
             $0.top.equalToSuperview().inset(16)
             $0.leading.trailing.equalToSuperview().inset(16)
         }
     }
-
-    
 }
