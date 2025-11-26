1a391b348a98657c6d5c2487707be6b9e85b79ad
JaeSunEo <wotjs020708@gmail.com>
2025-11-26 12:18:30 +0900

    [fix/#61] final 추가

--- KORAILTALK-iOS/KORAILTALK-iOS/Presentation/Reservation/View/ReservationListCell.swift
+++ KORAILTALK-iOS/KORAILTALK-iOS/Presentation/Reservation/View/ReservationListCell.swift
@@ -10,7 +10,7 @@ import UIKit
 import SnapKit
 import Then
 
-class ReservationListCell: UICollectionViewCell,ReuseIdentifiable {
+final class ReservationListCell: UICollectionViewCell,ReuseIdentifiable {
     
     //MARK: - UI
     
