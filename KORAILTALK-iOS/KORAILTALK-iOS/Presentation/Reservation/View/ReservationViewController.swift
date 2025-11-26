8ddc36e5840e355594af6e6a5506c5e2f3df31bf
JaeSunEo <wotjs020708@gmail.com>
2025-11-26 21:17:18 +0900

    [add/#77] ReservationViewController 추가

---
+++ KORAILTALK-iOS/KORAILTALK-iOS/Presentation/Reservation/View/ReservationViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ReservationViewController.swift
+//  KORAILTALK-iOS
+//
+//  Created by 어재선 on 11/26/25.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class ReservationViewController: BaseView {
+    
+    override func setUI() {
+        
+    }
+    
+    override func setStyle() {
+        
+    }
+    
+    override func setLayout() {
+        
+    }
+    
+}
