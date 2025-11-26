ac18015613a24e296e8d83885b0f218ab7b427d1
JaeSunEo <wotjs020708@gmail.com>
2025-11-27 03:17:41 +0900

    [fix/#83] DTO 만들기전 기존 모델 변경

--- KORAILTALK-iOS/KORAILTALK-iOS/Global/Enums/TrainNameType.swift
+++ KORAILTALK-iOS/KORAILTALK-iOS/Global/Enums/TrainNameType.swift
@@ -7,39 +7,40 @@
 
 import UIKit
 
+
 enum TrainNameType: String, CaseIterable {
-    case ktx
-    case srt
-    case itxSaemaeul
-    case itxMaeum
-    case mugunghwa
+    case ktx = "KTX"
+    case ktxSancheon = "KTX-S"
+    case ktxCheongryong = "KTX-C"
+    case itxSaemaeul = "ITX-N"
+    case itxMaeum = "ITX-M"
+    case mugunghwa = "FLOWER"
     
     var title: String {
         switch self {
         case .ktx:
             return "KTX"
-        case .srt:
-            return "SRT"
+        case .ktxSancheon:
+            return "KTX-산천"
+        case .ktxCheongryong:
+            return "KTX-청룡"
         case .itxSaemaeul:
             return "ITX-새마을"
         case .itxMaeum:
             return "ITX-마음"
         case .mugunghwa:
-            return "무궁화"
+            return "무궁화호"
         }
     }
     
     var backgroundColor: UIColor {
         switch self {
-        case .ktx:
+        case .ktx, .ktxSancheon, .ktxCheongryong:
             return .primary700
-        case .srt:
-            return .secondaryP400
         case .itxSaemaeul, .itxMaeum:
             return .secondaryM400
         case .mugunghwa:
             return .pointOrange
-            
         }
     }
     
@@ -50,5 +51,24 @@ enum TrainNameType: String, CaseIterable {
             return self.backgroundColor
         }
     }
+    
+    // 서버 값 → Enum 변환
+    init(rawValue: String) {
+        switch rawValue {
+        case "KTX":
+            self = .ktx
+        case "KTX-S":
+            self = .ktxSancheon
+        case "KTX-C":
+            self = .ktxCheongryong
+        case "ITX-N":
+            self = .itxSaemaeul
+        case "ITX-M":
+            self = .itxMaeum
+        case "FLOWER":
+            self = .mugunghwa
+        default:
+            self = .ktx
+        }
+    }
 }
-
